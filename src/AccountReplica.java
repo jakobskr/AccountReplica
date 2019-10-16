b3170b61ea2028767cd754729aaa68b920c876ba
jakobskr <jakobskr@ifi.uio.no>
2019-10-16 16:11:41 +0200

    yes

--- src/AccountReplica.java
+++ src/AccountReplica.java
@@ -1,6 +1,10 @@
+import java.util.List;
+import java.util.Vector;
 import java.io.InterruptedIOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import spread.*;
 
@@ -11,6 +15,28 @@ public class AccountReplica {
 	SpreadConnection connection;
 	SpreadGroup group;
 	double balance = 0.0;
+	ArrayList<Transaction> outstanding_collection; 
+	ArrayList<Transaction> executed_list;
+	
+	int order_counter = 0;
+	int outstanding_counter = 0;
+	
+	
+	protected static final int UNRELIABLE_MESS        = 0x00000001;
+	protected static final int RELIABLE_MESS          = 0x00000002;
+	protected static final int FIFO_MESS              = 0x00000004;
+	protected static final int CAUSAL_MESS            = 0x00000008;
+	protected static final int AGREED_MESS            = 0x00000010;
+	protected static final int SAFE_MESS              = 0x00000020;
+	protected static final int REGULAR_MESS           = 0x0000003f;
+	protected static final int SELF_DISCARD           = 0x00000040;
+	protected static final int REG_MEMB_MESS          = 0x00001000;
+	protected static final int TRANSITION_MESS        = 0x00002000;
+	protected static final int CAUSED_BY_JOIN         = 0x00000100;
+	protected static final int CAUSED_BY_LEAVE        = 0x00000200;
+	protected static final int CAUSED_BY_DISCONNECT   = 0x00000400;
+	protected static final int CAUSED_BY_NETWORK      = 0x00000800;
+	protected static final int MEMBERSHIP_MESS        = 0x00003f00;
 	
 	public AccountReplica() {
 		
@@ -20,6 +46,8 @@ public class AccountReplica {
 		this.account_name = an;
 		this.server_adress = sa;
 		this.number_of_replicas = n;
+		outstanding_collection = new ArrayList<Transaction>(); 
+		executed_list = new ArrayList<Transaction>();
 	}
 	
 	public String toString() {
@@ -39,7 +67,7 @@ public class AccountReplica {
 		
 		ar.connection = new SpreadConnection();
 		try {
-			ar.connection.connect(InetAddress.getByName(ar.server_adress),4803,id,false,false);
+			ar.connection.connect(InetAddress.getByName(ar.server_adress),4803,id,false,true);
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -51,7 +79,7 @@ public class AccountReplica {
 		ar.group = new SpreadGroup();
 		
 		try {
-			ar.group.join(ar.connection,"group");
+			ar.group.join(ar.connection,ar.account_name);
 		} catch (SpreadException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -60,36 +88,90 @@ public class AccountReplica {
 		System.out.println("did it work?");
 		System.out.println(ar);
 		
-		
+		//ar.sendJoinMessage();
+		ar.sendMessage();
 		ar.waitForOthers();
-		
-		try {
-			ar.group.leave();
-		} catch (SpreadException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		try {
-			ar.connection.disconnect();
-		} catch (SpreadException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		
+		ar.exit();
 		System.out.println("awoo");
 		
 	}
 	
+	public void sendMessage() {
+		byte[] a = "doot".getBytes();
+		SpreadMessage message = new SpreadMessage();
+		message.setData(a);
+		message.addGroup(this.account_name);
+		message.isUnreliable();
+		
+		try {
+			connection.multicast(message);
+		} catch (SpreadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public SpreadMessage waitForOthers() throws InterruptedIOException, SpreadException {
 		int others = 1;
+		int i = 0;		
 		
-		System.out.println("waiting for something to happen :)");
-		SpreadMessage message= this.connection.receive();
-		System.out.println(message);
+		while ("hei".length() == 3) {
+			System.out.println("waiting for something to happen! i = " + i);
+			SpreadMessage message= this.connection.receive();
+						
+			if (message.isRegular()) {
+				System.out.println("recieved a regular message");
+				System.out.println(message.getSender());
+			}
+			
+			else if (message.isMembership()) {
+				System.out.println("recieved memberhship message :) " + message.getMembershipInfo().getMembers().length);
+			}
+			
+			else {
+				System.out.println(message.getMembershipInfo().getGroup());
+			}
+			i++;
+			
+			
+		}
 		
-		return message;
+		
+		
+		System.out.println("somehting happened");
+		return null;
 	}
+	
+	public void sendJoinMessage() {
+		SpreadMessage message = new SpreadMessage();
+		message.addGroup(this.account_name);
+		message.setSelfDiscard(true);
+		
+		try {
+			this.connection.multicast(message);
+		} catch (SpreadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(connection.isConnected());
+	}
+	
+	public void exit() {
+		
+		try {
+			group.leave();
+		} catch (SpreadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			connection.disconnect();
+		} catch (SpreadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 }
